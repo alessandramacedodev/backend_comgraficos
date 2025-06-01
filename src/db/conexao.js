b1d82f1f2c6144ea31f595a81fba5af891d29e15
Alessandra Macedo <alessandramacedo.dev@gmail.com>
2025-06-01 13:19:29 -0300

    conexao.js

--- src/db/conexao.js
+++ src/db/conexao.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB)
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log('Conexão feita com sucesso com o MongoDB');
     } catch (err) {
         console.log('Error de conexão com o MongoDB', err.message);
